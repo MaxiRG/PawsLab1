bed592b85cb971bb587022f7c2d74df3466d3151
“MaxiRG” <maxiramirezgis@gmail.com>
2023-03-22 11:15:07 -0300

    home and refactor login

--- paws-front/src/pages/home/Home.js
+++ paws-front/src/pages/home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {get} from "../../utils/http";
+import {useState} from "@types/react";
+import {Login} from "../../components/Login";
+import {Register} from "../../components/Register";
 
-const Home = (props) => (
-    <div>
-        <h1>
-            hello world
-        </h1>
-    </div>
-);
+const Home = (props) => {const [currentForm, setCurrentForm] = useState('login');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+    }
+
+    return (
+        <div className="App">
+            {
+                currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+            }
+        </div>
+    );
+};
 
 export default Home;
