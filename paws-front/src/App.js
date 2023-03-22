b6eccb089f4bf04046187b3c32377ca7e49e49ba
tomassfreile <tomas.freile@ing.austral.edu.ar>
2023-03-22 09:30:15 -0300

    login and register

--- paws-front/src/App.js
+++ paws-front/src/App.js
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
 import './App.css';
-import Router from "./utils/router/Router";
+import { Login } from "./Login";
+import { Register } from "./Register";
+
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <div>
-      <Router/>
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
 
 export default App;
+
+
+
