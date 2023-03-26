7f14b79724317ec805ec034428a5e0087405ba1b
tomassfreile <tomas.freile@ing.austral.edu.ar>
2023-03-26 18:09:30 -0300

    file changes

--- paws-front/src/index.js
+++ paws-front/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './pages/Login/Login.css'
-import Login from './pages/Login/Login';
+import './styles/Login.css'
+import Login from './pages/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
