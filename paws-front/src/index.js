8db2ae1d419171e4a2d0bf67325a3b4ccb57fb64
tomassfreile <tomas.freile@ing.austral.edu.ar>
2023-03-26 18:14:20 -0300

    router fix

--- paws-front/src/index.js
+++ paws-front/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/Login.css'
-import Login from './pages/Login';
+import Router from './utils/router/Router';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Router />
   </React.StrictMode>
 );
 
