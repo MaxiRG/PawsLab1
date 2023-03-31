e42902c23051961fa3a3cb9a6a456cc42aac5270
tomassfreile <tomas.freile@ing.austral.edu.ar>
2023-03-31 12:26:30 -0300

    register and http changes

--- paws-front/src/components/Login.jsx
+++ paws-front/src/components/Login.jsx
@@ -1,15 +1,49 @@
 import React, { useState } from "react";
 import logo from '../images/logo.jpg'
+import { post } from "../utils/http";
+import { useNavigate } from "react-router-dom";
 
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(email);
-    }
+    
+        if (!email.trim()) {
+          alert('Please enter an email');
+          return;
+        }
+      
+        if (!password.trim()) {
+          alert('Please enter a password');
+          return;
+        }
+         
+        // Create a new user object with the form data
+        const user = {
+          email: email,
+          password: password,
+        };
+    
+        try {
+          const response = await post('/login', user);
+    
+          if (response.ok) {
+            navigate("/");
+          } else {
+            // Registration failed, display an error message
+            const data = await response.json();
+            setErrorMessage(data.message);
+            errorMessage.textContent = 'Login failed. Please try again later.';
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
         <div className="auth-form-container">
@@ -22,10 +56,11 @@ export const Login = (props) => {
                 <label htmlFor="email">EMAIL</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="" id="email" name="email" />
                 <label htmlFor="password">PASSWORD</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password"  id="password" name="password" />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"  id="password" name="password" />
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+            {errorMessage && <div id="error-message">{errorMessage}</div>}
         </div>
     )
 }
