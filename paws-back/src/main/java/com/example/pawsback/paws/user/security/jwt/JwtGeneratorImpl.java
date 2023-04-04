a3e0d4220791ae4f7d21ed9a329324eff41cb732
tomassfreile <tomas.freile@ing.austral.edu.ar>
2023-04-04 12:32:49 -0300

    added role to token, getters and setters to user

--- paws-back/src/main/java/com/example/pawsback/paws/user/security/jwt/JwtGeneratorImpl.java
+++ paws-back/src/main/java/com/example/pawsback/paws/user/security/jwt/JwtGeneratorImpl.java
@@ -1,6 +1,7 @@
 package com.example.pawsback.paws.user.security.jwt;
 
 import com.example.pawsback.paws.user.model.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -12,21 +13,39 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @Service
 public class JwtGeneratorImpl implements JwtGeneratorInterface {
+
         @Value("${app.jwttoken.message}")
         private String message;
 
-        static public final SecretKey KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        static final SecretKey KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
         @Override
         public Map<String, String> generateToken(User user) {
-                String jwtToken="";
-                jwtToken = Jwts.builder().setSubject(user.getEmail()).setIssuedAt(new Date()).signWith(KEY).compact();
+                Map<String, Object> claims = new HashMap<>();
+                claims.put("sub", user.getEmail());
+                claims.put("role", user.getRole());
+
+                String jwtToken = Jwts.builder()
+                        .setClaims(claims)
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // token valid for 24 hours
+                        .signWith(KEY)
+                        .compact();
+
                 Map<String, String> jwtTokenGen = new HashMap<>();
                 jwtTokenGen.put("token", jwtToken);
                 jwtTokenGen.put("message", message);
+
                 return jwtTokenGen;
+        }
+
+        public Claims parseToken(String token) {
+                return Jwts.parserBuilder()
+                        .setSigningKey(KEY)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
         }
 }
